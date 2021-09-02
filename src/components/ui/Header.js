74b17e419804b8fcbecc46b6f22c565ac302abb8
ABHISHEKA A M <76518816+AMABHISHEKA@users.noreply.github.com>
2021-09-02 15:31:40 +0530

    Initial commit
    Breaking Bad Cast API displaying using React.js

---
+++ src/components/ui/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import logos from "../../img/logo.png"
+
+const Header = () => {
+    return (
+        <div className='header-brand'>
+            <img src={logos} alt='' />
+        </div>
+    )
+}
+
+export default Header
